499f7461956ec6283ea025760f38c396f3155aac
Vikram Kumar <vikram.kumar@aircanada.ca>
2022-12-25 18:05:22 -0700

    fixed styling and added css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,22 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import { MaterialUISwitch } from '../UI/MUItoggle';
 
+//COMPONENTS
+import MenuBar from '../UI/MenuBar';
+import Logo from './Logo';
+import Contact from './Contact';
+
 //CSS
 import styles from './Component.module.css';
 
-//UI
-import MenuBar from '../UI/MenuBar';
-import Logo from './Logo';
+//REDUX
+import { useSelector } from 'react-redux';
 
 const Header = ({ setTheme }) => {
+  const theme = useSelector((state) => state.global.theme);
+  const dark = styles.menuitem_dark;
+  const light = styles.menuitem_light;
+
   return (
     <div>
       <Grid container spacing={3}>
@@ -20,17 +28,21 @@ const Header = ({ setTheme }) => {
         </Grid>
         <Grid item xs={6}>
           <MenuBar>
-            <div className={styles.menuitem}>Home</div>
-            <div className={styles.menuitem}>Tentang</div>
-            <div className={styles.menuitem}>Persyaratan</div>
-            <div className={styles.menuitem}>Testimoni</div>
+            <div className={theme ? dark : light}>Home</div>
+            <div className={theme ? dark : light}>Tentang</div>
+            <div className={theme ? dark : light}>Persyaratan</div>
+            <div className={theme ? dark : light}>Testimoni</div>
           </MenuBar>
         </Grid>
-        <Grid item xs>
-          <div className={styles.toggle_button}>
+        <Grid item xs={2}>
+          <div className={styles.contact}>
+            <Contact />
+          </div>
+          <div className={styles.switch}>
             <MaterialUISwitch
               sx={{ m: 1 }}
               onClick={(event) => setTheme(event.target.checked)}
+              className={styles.toggle_button}
             />
           </div>
         </Grid>
